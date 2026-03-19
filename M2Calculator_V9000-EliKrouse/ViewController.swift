ab3365432cd1ee4f45146961dcdd309dec2dd31b
Eli-Krouse <williamelijahkrouse@gmail.com>
2026-03-19 11:34:17 -0400

    Initial Commit

---
+++ M2Calculator_V9000-EliKrouse/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  M2Calculator_V9000-EliKrouse
+//
+//  Created by Krouse, William E. on 2/24/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var operand1TextField: UITextField!
+    @IBOutlet weak var operand2TextField: UITextField!
+    @IBOutlet weak var selectOperator: UIButton!
+    
+    @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func operatorButton(_ sender: Any)
+    {
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "+", style: .default ) {_ in self.selectOperator.setTitle("+", for: .normal)})
+        actionSheet.addAction( UIAlertAction(title: "-", style: .default ) {_ in self.selectOperator.setTitle("-", for: .normal)})
+        actionSheet.addAction( UIAlertAction(title: "*", style: .default ) {_ in self.selectOperator.setTitle("*", for: .normal)})
+        actionSheet.addAction( UIAlertAction(title: "/", style: .default ) {_ in self.selectOperator.setTitle("/", for: .normal)})
+    }
+    
+    func showAlert(_ myMessage: String)
+    {
+        let alert = UIAlertController(title: "Error", message: myMessage, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    @IBAction func calculate(_ sender: Any)
+    {
+        guard let op = selectOperator.title(for: .normal), op != "?" else
+        {
+            showAlert("Please select a valid operator")
+            return
+            
+        }
+        
+        guard let a = operand1TextField.text, let op1 = Int(a) else
+        {
+            showAlert("Please input a number for operand 1.")
+            return
+            
+        }
+        guard let b = operand2TextField.text, let op2 = Int(b) else
+        {
+            showAlert("Please input a number for operand 2.")
+            return
+            
+        }
+        var result : Int? = nil
+            
+        switch op
+        {
+        case "+":
+            result = op1 + op2
+        case "-":
+            result = op1 - op2
+        case "*":
+            result = op1 + op2
+        case "/":
+            result = op1 - op2
+        default:
+            print("Invalid Operand")
+        }
+        guard let result = result else
+        {
+            return
+        }
+        resultLabel.text = "\(result)"
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
